b0641eb74fc6eb1f2d4c8eb45107b202f72bba2e
nateyoungren <nathanaelyoungren@gmail.com>
2019-02-05 12:33:16 -0800

    Setup storyboard and connect view to class and vc.

---
+++ StarRating/StarRating/StarRating.swift
@@ -0,0 +1,17 @@
+//
+//  StarRating.swift
+//  StarRating
+//
+//  Created by Nathanael Youngren on 2/5/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import UIKit
+
+class StarRating: UIControl {
+
+    var value: Int = 1
+    
+    
+
+}
---
+++ StarRating/StarRating/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  StarRating
+//
+//  Created by Nathanael Youngren on 2/5/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+    }
+    
+    @IBAction func updateRating(_ sender: StarRating) {
+        
+    }
+    
+
+}
+
